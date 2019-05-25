f9bf176f876189ebdf1a775dca1cc250202fb644
The Natural <philip.smalls86@gmail.com>
2019-05-25 01:29:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2 
   array.join(" and ")
   elsif array.length ==3
-  array.join(", ") << ", #{array.length - 1}"
+  array.join(", ") << ", and #{array.length - 1}"
   end
 end 
