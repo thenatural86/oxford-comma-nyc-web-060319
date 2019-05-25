b2954270f9f04ff494178b884fbd3edc9e8e159f
The Natural <philip.smalls86@gmail.com>
2019-05-25 01:31:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
-end
+  if array.length == 1
+    array.join()
+  elsif array.length == 2 
+  array.join(" and ")
+  elsif array.length >=3
+  array[0...-1].join(", ") << ", and #{array[- 1]}"
+  end
+end 
